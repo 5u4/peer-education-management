bdee51e6ce011545def2ad2ae50ddef08cb9b039
Senhung Wong <alexwongsenhung@gmail.com>
2017-08-18 23:06:30 -0700

    Config Update

--- controllers/member.php
+++ controllers/member.php
@@ -1,15 +1,12 @@
 <?php
-include_once '../configs/config.php';
-
+include '../configs/config.php';
+?>
+<?php
 class Member {
-    private $student_id;
-    private $prefername;
-    private $firstname;
-    private $lastname;
-    private $section_time;
+
 
     public function get_section_time($id) {
-        $sql = "SELECT * FROM members WHERE $student_id=$id;";
+        $sql = "SELECT * FROM managers WHERE $manager_id=$id;";
         $result = mysqli_query($con, $sql);
         while ($row = mysqli_fetch_assoc($result)) {
             echo $row['section_time']."<br/>";

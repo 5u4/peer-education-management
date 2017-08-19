0155d49d7417962185c3d995d80fc7f4cd1b0bf4
David <mrycchou@gmail.com>
2017-08-18 22:45:33 -0700

    config.php

--- configs/config.php
+++ configs/config.php
@@ -5,7 +5,9 @@ define('DB_NAME', 'peer_education');
 define('DB_USER','root');
 define('DB_PASSWORD','qwe123');
 
-$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+$con = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+$db = mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+
+
 
 ?>

b5b8f20d234e11d43196270fb7ec70b82ae17d56
David <mrycchou@gmail.com>
2017-08-18 18:44:53 -0700

    login

---
+++ controllers/authentication.php
@@ -0,0 +1,47 @@
+<?php
+
+// https://www.tutorialspoint.com/php/php_mysql_login.htm
+
+class Authentication {
+  public login() {
+    echo "<?php include("config.php"); ?>";
+    echo "<?php session_start(); ?>"
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+    $sql = "SELECT id FROM managers WHERE username = '$myusername' and password = '$mypassword'";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
+      
+    $count = mysqli_num_rows($result);
+      
+    // If result matched $myusername and $mypassword, table row must be 1 row
+		
+    if($count == 1) {
+      session_register("myusername");
+      $_SESSION['login_user'] = $myusername;
+         
+      header("location: index.php");
+    }else {
+      $error = "Your Login Name or Password is invalid";
+    }
+    }
+}
+
+}
+
+}
+?>
+
+
+}
+
+
+
+
+
+?>

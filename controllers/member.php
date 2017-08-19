07a7fc9eafdf71e9291a84c1065ab327e8558b20
Senhung Wong <alexwongsenhung@gmail.com>
2017-08-18 21:21:29 -0700

    Member Class

--- controllers/member.php
+++ controllers/member.php
@@ -9,6 +9,17 @@ class Member {
     private $section_time;
 
     public function get_section_time($student_id) {
-        $student_id =
+        $sql = "SELECT * FROM  WHERE $student_id=?;";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            echo "SQL statement failed";
+        } else {
+            mysqli_stmt_bind_param($stmt, "c", $student_id);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row['user_uid']."<br />";
+            }
+        }
     }
 }

cfd6d66c46e49f7af1ba485ebb5de8b1394c6d54
Senhung Wong <alexwongsenhung@gmail.com>
2017-08-18 22:49:21 -0700

    Config Update

--- configs/config.php
+++ configs/config.php
@@ -5,8 +5,8 @@ define('DB_NAME', 'peer_education');
 define('DB_USER','root');
 define('DB_PASSWORD','qwe123');
 
-$con = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
-$db = mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysqli_error());
+$db = mysqli_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysqli_error());
 
 
 
--- controllers/member.php
+++ controllers/member.php
@@ -10,7 +10,7 @@ class Member {
 
     public function get_section_time($id) {
         $sql = "SELECT * FROM members WHERE $student_id=$id;";
-        $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($con, $sql);
         while ($row = mysqli_fetch_assoc($result)) {
             echo $row['section_time']."<br/>";
         }

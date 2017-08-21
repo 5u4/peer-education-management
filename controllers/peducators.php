65c3d8f42faf4caa50eaa271f052e595c4646340
David (Yi) <ycchou@sfu.ca>
2017-08-20 20:39:48 -0700

    Constructor for peducator class.

--- controllers/peducators.php
+++ controllers/peducators.php
@@ -19,8 +19,24 @@ class peducators {
 		$con = connection();
 
 		// send query to database
-		
-	}
+		$sql = "SELECT * FROM managers WHERE peducator_id=$peducator_id;";
+        	$result = mysqli_query($con, $sql);
+        	$row = mysqli_fetch_assoc($result);
+
+		// check if the peducator exists
+		if($row == 0) {
+			echo 'This peer educator does not exist.';
+			return 2;
+		}
+
+		// assign values to attributes
+		$this->peducator_id = $row['peducator_id'];
+        	$this->student_id = $row['student_id'];
+        	$this->preferred_name = $row['preferred_name'];
+       		$this->first_name = $row['first_name'];
+		$this->last_name = $row['last_name'];
+		$this->section_id = $row['section_id'];
+	} // end of __construct()
 
 
 }

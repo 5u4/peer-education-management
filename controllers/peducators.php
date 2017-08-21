d2ae787cb527e1315c658dbedebd5765340711c8
David (Yi) <ycchou@sfu.ca>
2017-08-20 20:31:13 -0700

    ok

--- controllers/peducators.php
+++ controllers/peducators.php
@@ -1,9 +1,26 @@
 <?php
+include_once '../configs/config.php';
+
 
 class peducators {
 
 	// Attributes
-	
+	private $peducator_id;
+	private $student_id;
+	private $preferred_name;
+	private $first_name;
+	private $last_name;
+	private $section_id;
+
+
+
+	function __construct($peducator_id) {
+		// connection to db
+		$con = connection();
+
+		// send query to database
+		
+	}
 
 
 }

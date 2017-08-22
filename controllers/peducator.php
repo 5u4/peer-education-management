1ddd6a7fff68c8b6041558477c539353142e8653
David <mrycchou@gmail.com>
2017-08-21 21:52:00 -0700

    PE set contributed mins.

--- controllers/peducator.php
+++ controllers/peducator.php
@@ -177,16 +177,96 @@ class Peducator {
 		return false;
 	}
 
-	public function set_section_id($sec_id) {
+	public function set_courses($cour_id) {
 
+		$sql = "INSERT INTO peducator_courses (peducator_id, course_id) 
+		VALUES ('$this->peducator_id','$cour_id')";
+
+		$result = mysqli_query($this->connect_to_db, $sql);
+
+		// If the result is false, there are two possibilities:
+		// (1) The course does not exist. Please check courses table.
+		// (2) This PE already has this course on record, please check PE_course table.
+		if($result) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public function set_courses() {
-		
+	public function delete_courses($cour_id) {
+		$sql = "DELETE FROM peducator_courses WHERE course_id=$cour_id";
+		$result = mysqli_query($this->connect_to_db, $sql);
+
+		if($result) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public function set_weekly_contributed_hours() {
+	public function set_contributed_mins($mins, $sec_id, $weeknum) {
+		//		
+		// Check if the data already exist in database.
+		//
+		$check_sql = "SELECT * FROM peducator_sections 
+		WHERE peducator_id = '$this->peducator_id' 
+		AND section_id = $sec_id 
+		AND week_number = $weeknum";
+
+		$check_result = mysqli_query($this->connect_to_db, $check_sql);
 		
+		if (!$check_result) {
+			die(mysqli_error($this->connect_to_db));
+		}
+			
+		//
+		// If the data already exist, we update the data.
+		// If the data does not exist, we insert new data.
+		//
+
+		if(mysqli_num_rows($check_result) != 0) {
+			//
+			// If the data exist.
+			//
+
+			$sql = "UPDATE peducator_sections 
+			SET contributed_mins='$mins' 
+			WHERE peducator_id=$this->peducator_id 
+			AND section_id = $sec_id 
+			AND week_number = $weeknum;";
+
+			$result = mysqli_query($this->connect_to_db, $sql);
+
+			if (!$result) {
+				die( mysqli_error($this->connect_to_db));
+			}
+			if($result) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			//
+			// If the data does not exist.
+			//
+
+			$sql = "INSERT INTO peducator_sections 
+			(peducator_id, section_id, week_number, contributed_mins) 
+			VALUES ('$this->peducator_id','$sec_id', '$weeknum', '$mins')";
+
+			$result = mysqli_query($this->connect_to_db, $sql);
+
+			if (!$result) {
+				die( mysqli_error($this->connect_to_db));
+			}
+
+			if($result) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 } // end of peducators class
--- test/david_test.php
+++ test/david_test.php
@@ -1,5 +1,4 @@
 <?php
-echo 'Test';
 
 include_once '../controllers/peducator.php';
 include_once '../controllers/course.php';
@@ -8,8 +7,8 @@ include_once '../controllers/section.php';
 
 $pe = get_peducator(1);
 
-foreach ($pe->get_all_sections() as $value) {
-	echo $value->get_section_id();
+if($pe->set_contributed_mins(999,99,99)) {
+	echo'Mins updated';
 }
 
 

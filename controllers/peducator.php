4ed22813045aba2ef5ff89b98accbe6c6ce92458
David <mrycchou@gmail.com>
2017-08-21 18:36:54 -0700

    semicolon

--- controllers/peducator.php
+++ controllers/peducator.php
@@ -1,6 +1,6 @@
 <?php
 include_once '../configs/config.php';
-include_once '../controllers/course.php'
+include_once '../controllers/course.php';
 
 // ----------------------
 // peducators description

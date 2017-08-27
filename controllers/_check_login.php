4b703404bdfebd844303c53494984b17a0566ae7
David <mrycchou@gmail.com>
2017-08-27 16:24:13 -0700

    ok

--- controllers/_check_login.php
+++ controllers/_check_login.php
@@ -4,7 +4,7 @@ include_once '../controllers/authentication.php';
 session_start();
 
 if(!isset($_SESSION['manager_id'])) {
-	header('Location: ../views/login.php');
+	header('Location:' . $_SERVER['DOCUMENT_ROOT']. '/views/login.php');
 } else {
 	$mid = (int)$_SESSION['manager_id'];
 	$_SESSION['current_user'] = get_manager($mid);
